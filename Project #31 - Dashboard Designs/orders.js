ebdd74b54abdb5a054eed88a14a22e442f5d4c0f
222bakteri <80977750+permenkojek@users.noreply.github.com>
2023-10-08 18:36:25 +0700

    Adding Project #31

---
+++ Project #31 - Dashboard Designs/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Lemari Baju Anak',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Kasur Tidur',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Meja Makan',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
